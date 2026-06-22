c89fc72062865b0bc8f5283718614b5fae405c42
saraalhamede <saraslemanalhamede@gmail.com>
2026-06-22 19:15:32 +0300

    updtes

--- studest development/src/data/material.js
+++
@@ -1,9 +0,0 @@
-const material=[{
-    id:1,
-    courseId:1,
-    title:"Lecture 1: Introduction to Programming",
-    description:"In this lecture, we will cover the basics of programming and introduce Python.",
-    type:"video",
-    url:"https://www.youtube.com/watch?v=rfscVS0vtbw",
-    uploadBy:"Jane Smith",
-}]
---
+++ studest-development/README.md
@@ -0,0 +1,3 @@
+# Student Development 
+ Student Development is a simple REact mid-project for an education center. It ihas seprate student and teacher dashboards, course pages, learning materails, assignments, announcement,and local teacher forms.
+
